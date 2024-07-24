a15d4a00e9ef7abca6b28e74124e102a446b5f3a
Rodrigo Guerra <i220096@inete.net>
2024-07-24 10:55:28 +0100

    1 commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
+
+function toggleMenu() {
+    document.getElementById("dropdownContent").classList.toggle("show");
+}
+
+// Fechar o menu se o usuário clicar fora dele
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}       
